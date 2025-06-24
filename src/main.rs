3290c7b11b0ab708fd4500d190caf3484ad593c0
Abdul Rahman Sibahi <a.sibahi@gmail.com>
2025-06-24 20:33:48 +0300

    mvp
    so many dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use rand::{SeedableRng, seq::IteratorRandom};
+use std::{
+	fs::File,
+	io::{BufRead, BufReader},
+};
+use terminal_size::terminal_size;
+
 fn main() {
-    println!("Hello, world!");
+	let path = dirs::home_dir().unwrap().join(".prayers");
+	let file = File::open(path).expect("a .prayers file in ~ is expected and not found");
+
+	let b = BufReader::new(file);
+	let mut rng = rand::rngs::SmallRng::from_os_rng();
+
+	let width = terminal_size().unwrap().0.0;
+
+	let mut line = b.lines().choose(&mut rng).unwrap().unwrap();
+
+	let mut args = pico_args::Arguments::from_env();
+	if args.contains("--rev") {
+		line = line.chars().rev().collect();
+	}
+	println!("{line:>0$}", width as usize);
 }
