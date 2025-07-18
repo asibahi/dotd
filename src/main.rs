2a6beaf722ed4765393416598b50d46ab742ded6
Abdul Rahman Sibahi <a.sibahi@gmail.com>
2025-07-19 01:31:12 +0300

    add help

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use std::{
 use terminal_size::terminal_size;
 
 fn main() {
+	let mut args = pico_args::Arguments::from_env();
+	if args.contains(["-h", "--help"]) {
+		print_help();
+	}
+
 	let path = dirs::home_dir().unwrap().join(".prayers");
 	let file = File::open(path).expect("a .prayers file in ~ is expected and not found");
 
@@ -16,9 +21,18 @@ fn main() {
 
 	let mut line = b.lines().choose(&mut rng).unwrap().unwrap();
 
-	let mut args = pico_args::Arguments::from_env();
 	if args.contains("--rev") {
 		line = line.chars().rev().collect();
 	}
 	println!("{line:>0$}", width as usize);
+}
+
+fn print_help() {
+	println!("Print a dua, read from $HOME/.prayers\n");
+	println!("OPTIONS:");
+	println!(
+		"{:>6} {:<6}\treverse the text. useful for e.g. Ghostty",
+		"", "--rev"
+	);
+	println!("{:>6} {:<6}\tprint this message", "-h", "--help");
 }
