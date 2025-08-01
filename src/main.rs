e5275150e414693f5d71fd5ce2adc974114e318b
Abdul Rahman Sibahi <a.sibahi@gmail.com>
2025-08-01 22:34:39 +0300

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,49 @@ use std::{
 	fs::File,
 	io::{BufRead, BufReader},
 };
-use terminal_size::terminal_size;
+use terminal_size::{Width, terminal_size};
 
 fn main() {
 	let mut args = pico_args::Arguments::from_env();
 	if args.contains(["-h", "--help"]) {
 		print_help();
+		std::process::exit(0)
 	}
 
-	let path = dirs::home_dir().unwrap().join(".prayers");
-	let file = File::open(path).expect("a .prayers file in ~ is expected and not found");
+	let Some((Width(width), _)) = terminal_size() else {
+		failure("Not a terminal");
+	};
+	let Some(file) = dirs::home_dir()
+		.map(|p| p.join(".prayers"))
+		.and_then(|p| File::open(p).inspect_err(|e| eprintln!("{e}")).ok())
+	else {
+		failure("A .prayers file in the home directory is expected and not found");
+	};
 
 	let b = BufReader::new(file);
 	let mut rng = rand::rngs::SmallRng::from_os_rng();
 
-	let width = terminal_size().unwrap().0.0;
+	let Some(Ok(mut line)) = b.lines().choose(&mut rng) else {
+		failure("The .prayers file must not be empty")
+	};
 
-	let mut line = b.lines().choose(&mut rng).unwrap().unwrap();
-
-	if args.contains("--rev") {
+	if args.contains("--rev") || args.contains("--reverse") {
 		line = line.chars().rev().collect();
 	}
 	println!("{line:>0$}", width as usize);
 }
 
 fn print_help() {
+	let s = 3;
+	let l = 9;
+
 	println!("Print a dua, read from $HOME/.prayers\n");
 	println!("OPTIONS:");
-	println!(
-		"{:>6} {:<6}\treverse the text. useful for e.g. Ghostty",
-		"", "--rev"
-	);
-	println!("{:>6} {:<6}\tprint this message", "-h", "--help");
+	println!("{:>s$} {:<l$}\treverse the whole text. useful for e.g. Ghostty", "", "--rev");
+	println!("{:>s$} {:<l$}\tprint this message", "-h", "--help");
+}
+
+fn failure(msg: &str) -> ! {
+	eprintln!("{msg}");
+	std::process::exit(1)
 }
